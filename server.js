b4b6d6ffa44f3a402d506b8b9e9566a4365485cd
gerardgal <gerardodgalicia@gmail.com>
2021-07-26 17:28:58 -0300

    Refactoring

--- server.js
+++ server.js
@@ -1,57 +1,29 @@
 // For authentication feature use dotenv
 const sqlite3 = require('sqlite3');
 const express = require('express');
-
-const sqlite = sqlite3.verbose();
-
-// Check for let declaration
-// Connecting the database and opening it from memory
-let db = new sqlite.Database('./database/budget.db', sqlite.OPEN_READWRITE, (error) => {
-	if (error) {
-		return console.error(error.message);
-	}
-	console.log('Successfully connected to the movements database.');
-});
-
-// There are three opening modes for the DB
-// sqlite.OPEN_READONLY
-// sqlite.OPEN_READWRITE
-// sqlite.OPEN_CREATE
-
-// Creating a table
-// db.run('CREATE TABLE Programing(Langs)');
-
-// Inserting rows
-
-/* db.run('INSERT INTO Programing(Langs) VALUES(?)', ['C++'], (error) => {
-	if (error) {
-		return console.log(error.message);
-	}
-	// Getting the last insert id
-	console.log(`A row has been inserted! The rowid is ${this.lastID}. Rows inserted: ${this.changes}`);
-	console.log(this.lastID);
-});
-*/
-
-// Closing the database connection
-db.close((error) => {
-	if (error) {
-		return console.error(error.message);
-	}
-	console.log('Database connection closed');
-});
-
-
+const bodyParser = require('body-parser');
+const financeController = require('./controller/financeController');
 
 // Building the server
 const app = express();
+const path = require('path');
 const port = 3000;
+
+// Rendering HTML
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.set('views', './views');
+
+// Middleware (Parser)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/finance', financeController);
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/views/index.html');
+});
 
 app.listen(port, () => {
 	console.log('Hello! Im listening...');
 });
 
-// Testing requests
-app.get('/', (req, res) => {
-	res.send('Hello! Im listening...');
-});
+// Implement REST
